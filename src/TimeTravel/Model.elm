6742107502a5cc9d240ca872c3bc08f62fc73327
torii_y <torii_y@worksap.co.jp>
2016-06-04 16:29:11 +0900

    filter

--- src/TimeTravel/Model.elm
+++ src/TimeTravel/Model.elm
@@ -1,6 +1,9 @@
-module TimeTravel.Model exposing (Model) -- where
+module TimeTravel.Model exposing (Model, FilterOptions) -- where
 
 import TimeTravel.Util exposing (..)
 
-type alias Model model msg
-  = Nel (Maybe msg, model)
+type alias Model model msg =
+  Nel (Maybe msg, model)
+
+type alias FilterOptions =
+  List (String, Bool)
--- src/TimeTravel/View.elm
+++ src/TimeTravel/View.elm
@@ -6,6 +6,7 @@ import TimeTravel.Util exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+
 view : Model model msg -> Html msg -> Html msg
 view model original =
   div [] [ original, debugView model ]
@@ -14,30 +15,32 @@ view model original =
 debugView : Model model msg -> Html msg
 debugView (Nel current past) =
   div
-    [ style
-        [ ("position", "fixed")
-        , ("width", "250px")
-        , ("top", "0")
-        , ("right", "0")
-        , ("bottom", "0")
-        , ("background-color", "#444")
-        , ("color", "#eee")
-        ]
-    ]
-    [ modelView current
+    [ style debugViewStyle ]
+    [ headerView True True [("Tick", True)]
+    , modelView current
     , msgListView (List.filterMap fst (current :: past))
     ]
 
-panel : List (String, String)
-panel =
-  [ ("padding", "20px")
+
+headerView : Bool -> Bool -> FilterOptions -> Html msg
+headerView sync expand filterOptions =
+  div []
+  [ div [ style headerViewStyle ] [ text (toString sync), text (toString expand) ]
+  , filterView filterOptions
   ]
 
-modelViewStyle : List (String, String)
-modelViewStyle =
-  [ ("height", "100px")
-  , ("border-bottom", "solid 1px #666")
-  ] ++ panel
+
+filterView : FilterOptions -> Html msg
+filterView filterOptions =
+  div [ style filterViewStyle ] (List.map filterItemView filterOptions)
+
+
+filterItemView : (String, Bool) -> Html msg
+filterItemView (name, visible) =
+  div []
+    [ label [] [ input [ type' "checkbox", checked visible ] [], text name ]
+    ]
+
 
 modelView : (a, model) -> Html msg
 modelView (_, model) =
@@ -46,9 +49,47 @@ modelView (_, model) =
 
 msgListView : List m -> Html msg
 msgListView msgList =
-  div [ style panel ] (List.map msgView msgList)
+  div [ style panelStyle ] (List.map msgView msgList)
 
 
 msgView : m -> Html msg
 msgView msg =
   div [] [ text (toString msg) ]
+
+
+-- Styles
+
+panelStyle : List (String, String)
+panelStyle =
+  [ ("padding", "20px")
+  ]
+
+panelBorderStyle : List (String, String)
+panelBorderStyle =
+  [ ("border-bottom", "solid 1px #666")
+  ]
+
+debugViewStyle : List (String, String)
+debugViewStyle =
+  [ ("position", "fixed")
+  , ("width", "250px")
+  , ("top", "0")
+  , ("right", "0")
+  , ("bottom", "0")
+  , ("background-color", "#444")
+  , ("color", "#eee")
+  ]
+
+filterViewStyle : List (String, String)
+filterViewStyle =
+  [ ("background-color", "#333") ]
+  ++ panelBorderStyle ++ panelStyle
+
+headerViewStyle : List (String, String)
+headerViewStyle =
+  panelStyle
+
+modelViewStyle : List (String, String)
+modelViewStyle =
+  [ ("height", "100px") ]
+  ++ panelBorderStyle ++ panelStyle
