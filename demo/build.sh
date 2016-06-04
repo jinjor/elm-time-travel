07e10acf5f89f5cd144e5002d838eb6939e79aef
torii_y <torii_y@worksap.co.jp>
2016-06-04 15:53:21 +0900

    add example

--- demo/build.sh
+++ demo/build.sh
@@ -1,1 +1,3 @@
-elm-make Counter.elm --output=../index.html
+# elm-make Counter.elm --output=../index.html
+# elm-make DragAndDrop.elm --output=../index.html
+elm-make Clock.elm --output=../index.html
