b84cf581c8b26fd7b50f614f0486ddb686417757
torii_y <torii_y@worksap.co.jp>
2016-06-08 15:19:11 +0900

    cover most cases

--- src/TimeTravel/Internal/Parser.elm
+++ src/TimeTravel/Internal/Parser.elm
@@ -1,18 +1,107 @@
 module TimeTravel.Internal.Parser exposing (..) -- where
 
-import Parser exposing (separatedBy, symbol, token, Parser)
-import Parser.Char as PC
-import Parser.Number as PN
+import String
+import Parser exposing (separatedBy, symbol, token, (*>), (<*), optional
+  , Parser, or, satisfy, many, and, succeed, andThen, map, empty)
+import Parser.Char exposing (braced)
+-- import Parser.Number as PN
+
+type AST
+  = Record (List AST)
+  | Value String
+  | Property String AST
 
 parse : String -> Result String AST
 parse s = Parser.parse expression s
 
 expression : Parser AST
 expression =
-  Parser.map Value (token "1")
+  spaced record
+  `or`
+  spaced value
 
 
+value : Parser AST
+value =
+  map (Value << String.trim) <|
+    string (satisfy (\c -> c /= '=' && c /= '}' && c /= ','))
 
-type AST
-  = Record (List AST)
-  | Value String
+
+record : Parser AST
+record =
+  map Record <| braced properties
+
+
+properties : Parser (List AST)
+properties =
+  spaced (separatedBy property comma)
+
+property : Parser AST
+property =
+  (\_ key _ _ _ value _ -> Property key value)
+  `map`
+  spaces
+  `and`
+  propertyKey
+  `and`
+  spaces
+  `and`
+  equal
+  `and`
+  spaces
+  `and`
+  value
+  `and`
+  spaces
+
+
+propertyKey : Parser String
+propertyKey =
+  string (satisfy (\c -> not (isSpace c) && c /= '='))
+
+
+---
+
+
+-- spaced : Parser a -> Parser a
+-- spaced p =
+--   spaces *> p <* spaces
+
+spaced : Parser a -> Parser a
+spaced p =
+  (\_ v _ -> v)
+  `map`
+  spaces
+  `and`
+  p
+  `and`
+  spaces
+
+spaces : Parser String
+spaces = string space
+
+space : Parser Char
+space = satisfy isSpace
+
+noSpace : Parser Char
+noSpace = satisfy (not << isSpace)
+
+isSpace : Char -> Bool
+isSpace c =
+  c == '\t' ||
+  c == '\r' ||
+  c == '\n' ||
+  c == ' '
+
+string : Parser Char -> Parser String
+string char =
+  Parser.map String.fromList (many char)
+
+notEqual : Parser Char
+notEqual = satisfy ((/=) '=')
+
+comma : Parser Char
+comma = symbol ','
+
+equal : Parser Char
+equal = symbol '='
--- test/Tests.elm
+++ test/Tests.elm
@@ -1,6 +1,7 @@
 module Tests exposing (..) -- where
 
 import String
+import Parser as RawParser
 import TimeTravel.Internal.Parser as Parser exposing(..)
 import ElmTest exposing (..)
 
@@ -8,10 +9,45 @@ import ElmTest exposing (..)
 testParse : String -> AST -> Assertion
 testParse s ast = assertEqual (Ok ast) (Parser.parse s)
 
+testParseValue : String -> AST -> Assertion
+testParseValue s ast = assertEqual (Ok ast) (RawParser.parse Parser.value s)
+
+testParseRecord : String -> AST -> Assertion
+testParseRecord s ast = assertEqual (Ok ast) (RawParser.parse Parser.record s)
+
+testParseProperty : String -> AST -> Assertion
+testParseProperty s ast = assertEqual (Ok ast) (RawParser.parse Parser.property s)
+
+testParseProperties : String -> List AST -> Assertion
+testParseProperties s ast = assertEqual (Ok ast) (RawParser.parse Parser.properties s)
+
 tests : Test
 tests =
   suite "A Test Suite"
-    [ test "bracket" (testParse "1" (Value "1"))
+    [ test "value" (testParseValue "1" (Value "1"))
+    , test "value" (testParseValue "a" (Value "a"))
+    , test "value" (testParseValue "A" (Value "A"))
+    , test "value" (testParseValue "<function>" (Value "<function>"))
+    , test "value" (testParseValue "Tag (Tag a [])" (Value "Tag (Tag a [])"))
+    , test "value" (testParseValue "str:\" str \"" (Value "str:\" str \""))
+    , test "property" (testParseProperty "a=1" (Property "a" (Value "1")))
+    , test "property" (testParseProperty "a = 1" (Property "a" (Value "1")))
+    , test "properties" (testParseProperties "a=1" [Property "a" (Value "1")])
+    , test "properties" (testParseProperties "a=1,a=2" [Property "a" (Value "1"),Property "a" (Value "2")])
+    , test "properties" (testParseProperties "a=1 , a=2" [Property "a" (Value "1"),Property "a" (Value "2")])
+    , test "record1" (testParseRecord "{}" (Record []))
+    , test "record2" (testParseRecord "{ }" (Record []))
+    , test "record3" (testParseRecord "{a=1}" (Record [Property "a" (Value "1")]))
+    , test "expression1" (testParse "1" (Value "1"))
+    , test "expression2" (testParse " 1 " (Value "1"))
+    , test "expression3" (testParse "{}" (Record []))
+    , test "expression4" (testParse " {} " (Record []))
+    , test "expression5" (testParse "{ }" (Record []))
+    , test "expression6" (testParse "{a=1}" (Record [Property "a" (Value "1")]))
+    , test "expression7" (testParse "{ a = 1 }" (Record [Property "a" (Value "1")]))
+    , test "expression8" (testParse "{a=1,a=1}" (Record [Property "a" (Value "1"), Property "a" (Value "1")]))
+    , test "expression9" (testParse "{ a = 1 , a = 1 }" (Record [Property "a" (Value "1"), Property "a" (Value "1")]))
+    -- , test "expression10" (testParseValue "str:\" = {} \"" (Value "str:\" = {} \""))
     ]
 
 main : Program Never
